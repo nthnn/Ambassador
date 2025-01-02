5341970cc72c42b6d0bc58fff33cdc5983b1e3f6
Nathanne Isip <nathanneisip@gmail.com>
2025-01-03 03:54:24 +0800

    Package info for Ambassador main package path.

---
+++ src/web/ambassador/package-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of Ambassador.
+ *
+ * Ambassador is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Ambassador is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ambassador. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This is the package-level documentation for the web.ambassador package.
+ *
+ * It contains the core API for the Ambassador service.
+ *
+ *
+ * @since 1.0
+ */
+package web.ambassador;
