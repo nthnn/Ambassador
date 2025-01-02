99daa0def0a6ca6c859611d9cccbdf8f4f49e84b
Nathanne Isip <nathanneisip@gmail.com>
2025-01-03 04:03:44 +0800

    web.ambassador.util package information documentation page.

---
+++ src/web/ambassador/util/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 - Nathanne Isip
+ * This file is part of Ambassador.
+ *
+ * Ambassador is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Ambassador is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ambassador. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Utility classes for web ambassador services and data processing.
+ *
+ * @author <a href="https://github.com/nthnn" target="_blank">Nathanne Isip</a> &lt;<a href="mailto:nathanneisip@gmail.com">nathanneisip@gmail.com</a>&gt;
+ * @see web.ambassador.util.JsonUtil
+ */
+package web.ambassador.util;
